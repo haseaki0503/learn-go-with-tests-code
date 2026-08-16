16bba8fe7ba5dd37b0fa133915c06dcf0e25e174
Akira Hasegawa <contact@haseaki.com>
2026-08-16 14:45:40 +0900

    update

--- go-fundamentals/reflection/walk_test.go
+++ go-fundamentals/reflection/walk_test.go
@@ -43,6 +43,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"pointers to thins",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
 	}
 
 	for _, test := range cases {
